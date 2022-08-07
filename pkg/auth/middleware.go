43e0f98eaeb13fe946e707d7b6096e59b7f63dbb
valikhan.eltaev <valikhan.eltaev@gmail.com>
2022-08-07 15:05:58 +0600

    first commit

---
+++ pkg/auth/middleware.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"api-gateway-service/pkg/auth/pb"
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthMiddlewareConfig struct{
+	service *Service
+}
+
+func InitAuthMiddleware(srvc *Service) *AuthMiddlewareConfig{
+	return &AuthMiddlewareConfig{srvc}
+}
+
+func (a *AuthMiddlewareConfig) AuthRequired(c *gin.Context){
+	token, err := c.Cookie("token_auth")
+	if err != nil{
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	if token == ""{
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	res, err := a.service.Client.Validate(context.TODO(), &pb.ValidateRequest{Token: token})
+	if err != nil || res.Status != http.StatusOK{
+		c.AbortWithStatus(http.StatusUnauthorized)
+		return
+	}
+
+	c.Set("user_id", res.UserID)
+
+	c.Next()
+}
---
+++ pkg/auth/routes/sign_in.go
@@ -0,0 +1,34 @@
+package routes
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"api-gateway-service/pkg/auth/pb"
+)
+
+type SignInRequestBody struct{
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+func SignIn(c *gin.Context, client pb.AuthServiceClient) {
+	var req SignInRequestBody
+	err := c.BindJSON(&req)
+	if err != nil{
+		c.AbortWithError(http.StatusBadRequest, err)
+		return
+	}
+
+	res, err := client.SignIn(context.TODO(), &pb.SignInRequest{})
+	if err != nil{
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	c.SetCookie("token_auth", res.Token, 1, "", "", true, true)
+
+	
+}
